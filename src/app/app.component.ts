d2bbb4df27cfc0d1b0b8747b1d013ff6b20ce0d9
Wolfang corredor <you@example.com>
2022-03-02 23:11:20 -0500

    Commit pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  activar: boolean = false;
+  nombre: string = "Capitán América";
+  nombre2: string = "WolFAng CoRredor";
+  video:string  ='https://www.youtube.com/embed/FWtWJAmHuc8';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI : number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha : Date = new Date();
+  idioma :string = 'es';
+  valorPromesa = new Promise <string>((resolve) => {
+
+    setTimeout(()=>{
+      resolve('llego la data')
+    },3500);
+  });
+
+
+
+  heroe ={
+    nombre:'Logan',
+    clave: 'wolf',
+    edad: 500,
+    direccion: {
+      calle:'siempre viva',
+      casa: ' 123'
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+registerLocaleData(localEs);
+registerLocaleData(localFr);
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue :'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
